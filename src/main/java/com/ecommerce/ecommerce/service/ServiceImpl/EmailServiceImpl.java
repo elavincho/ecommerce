69717fd483d9773b689edfe2b8c481444ff793d1
elavincho <elavincho@gmail.com>
2024-07-02 22:26:17 -0300

    Mail Sender Funcionando ok

--- src/main/java/com/ecommerce/ecommerce/service/ServiceImpl/EmailServiceImpl.java
+++ src/main/java/com/ecommerce/ecommerce/service/ServiceImpl/EmailServiceImpl.java
@@ -13,7 +13,6 @@ import jakarta.mail.internet.MimeMessage;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
